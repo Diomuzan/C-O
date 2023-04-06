b6b6d3ff08c19f1c7dc45681c89c435afd8601fb
rincedal <94070451+Diomuzan247@users.noreply.github.com>
2023-04-06 23:00:45 +0200

    Initial commit

---
+++ SRC/C&O_Script.js
@@ -0,0 +1,45 @@
+const x = 6;
+document.getElementById = "De macht van 6 is 30"
+const y = 16;
+document.getElementById = "De macht van 16 is 256"
+let number = prompt ("Typ een getal in");
+if (state = "z") {
+    console.log(state);
+  }
+let age = prompt('Hoe oud bent u?');
+const year = 2021;
+console.log('Deze gebruiker komt uit', year - age, 'of' , year - age + 1)
+const v = 8;
+const w = 15;
+document.innerHTML = "15+8= 23"
+document.innerHTML ="15-8= 7"
+document.innerHTML = "15:8= 1,875"
+document.innerHTML = "15x8= 120"
+let random  = prompt('Typ een getal in alstublieft');
+alert(`Ik heb uw getal gedeeld door 2 en het antwoord is, ${random / 2}`)
+a = 5
+b = 6
+c = a + a + b;
+alert(c);
+d = a + a;
+alert(d);
+e = 4
+f = 1
+g = 2
+h = e + f + (e * g);
+alert(h);
+i = 'Hoi'
+j =  a + i;
+alert(j);
+k = 3
+l = k + g + (e * g) + a + f;
+alert(l)
+m = a + (a * k);
+alert(m)
+
+
+
+
+
+
+
